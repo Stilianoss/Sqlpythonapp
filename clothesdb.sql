f98025e4576737a2da5a998a00eded2c80495a42
Stilianoss <stelio_s1991@hotmail.com>
2025-03-28 20:02:20 +0200

    Add files via upload

---
+++ clothesdb.sql
@@ -0,0 +1,8 @@
+CREATE TABLE clothes
+(   id INT PRIMARY KEY AUTO_INCREMENT,
+    tittle VARCHAR(255),
+    category VARCHAR(100),
+    size VARCHAR (10),
+    color VARCHAR(50),
+    material VARCHAR(100),
+    );
